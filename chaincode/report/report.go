9273e37fd7b9b8debc79e4a6d739e70bf5c583b8
Siarhei Balashevich <balashevich@gmail.com>
2016-04-30 16:51:00 -0400

    revert back the changes

--- chaincode/report/report.go
+++ chaincode/report/report.go
@@ -34,48 +34,48 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 
 // Deletes an entity from state
 func (t *SimpleChaincode) settle(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	var err error
-	var total int
-	//var val, total, exchange_rate, amount int
+	//var err error
+	var key string
+	var val, total, exchange_rate, amount int
 
-	//exchange_rate = 13;
+	exchange_rate = 13;
 
-	keysIter, err := stub.RangeQueryState("1", "10")
-	if err != nil {
-		return nil, fmt.Errorf("keys operation failed. Error accessing state: %s", err)
-	}
-	//defer keysIter.Close()
-	//
-	//var keys []string
-	//var values []int
-	//total = 0;
-	//
-	for keysIter.HasNext() {
-		total = total + 1
-	//	key, _, err := keysIter.Next()
-	//	if err != nil {
-	//		return nil, fmt.Errorf("keys operation failed. Error accessing state: %s", err)
-	//	}
-	//	val, _ = strconv.Atoi(string(args[1]))
-	//
-	//	keys = append(keys, key)
-	//	values = append(values, val)
-	//	if(val > 0){
-	//		total = total + val;
-	//	}
-	//
-	}
-	//for index,name := range keys {
-	//	if(values[index] < 0){
-	//		amount = values[index]*-1*exchange_rate;
-	//		stub.QueryChaincode("2780b7463c57f343a9e107854c4b53150018cdd8fd74ca970c028de6bfa707f6e9f6cf2b20f0af4fdd04d2167651eb29c7bfabf19e6a93ae2aff65f55202d0e6", "change", []string{string(amount)})
-	//	}
-	//	name = name + "";
-	//	//err = stub.PutState(name, []byte(strconv.Itoa(0)));
-	//	//if err != nil {
-	//	//	return nil, errors.New("Meter cannot be updated")
-	//	//}
+	//keysIter, err := stub.RangeQueryState("1", "10")
+	//if err != nil {
+	//	return nil, fmt.Errorf("keys operation failed. Error accessing state: %s", err)
 	//}
+	//defer keysIter.Close()
+
+	var keys []string
+	var values []int
+	total = 0;
+
+	for i := 1; i < 10; i++ {
+		key = string(i)
+		value, err := stub.GetState(key)
+		if err != nil {
+			continue
+		}
+		val, _ = strconv.Atoi(string(value))
+
+		keys = append(keys, key)
+		values = append(values, val)
+		if(val > 0){
+			total = total + val;
+		}
+
+	}
+	for index,name := range keys {
+		if(values[index] < 0){
+			amount = values[index]*-1*exchange_rate;
+			stub.QueryChaincode("2780b7463c57f343a9e107854c4b53150018cdd8fd74ca970c028de6bfa707f6e9f6cf2b20f0af4fdd04d2167651eb29c7bfabf19e6a93ae2aff65f55202d0e6", "change", []string{string(amount)})
+		}
+		name = name + "";
+		//err = stub.PutState(name, []byte(strconv.Itoa(0)));
+		//if err != nil {
+		//	return nil, errors.New("Meter cannot be updated")
+		//}
+	}
 
 
 	return []byte(strconv.Itoa(total)), nil
