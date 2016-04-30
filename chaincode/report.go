8a0a2e4a076368fdc1b59641a4425516c169712c
Siarhei Balashevich <balashevich@gmail.com>
2016-04-30 12:17:09 -0400

    Initial version for Report contract with "init, report, query" methods

---
+++ chaincode/report.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// ReportChaincode implementation
+type ReportChaincode struct {
+}
+
+func (t *ReportChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	//var Name string    // Entities
+	var err error
+
+	if len(args) == 0 {
+		return nil, errors.New("Incorrect number of arguments. At least one Meter's name is required.")
+	}
+
+	for _,name := range args {
+		if len(name) == 0{
+			continue
+		}
+		err = stub.PutState(name, 0);
+		if err != nil {
+			return nil, errors.New("Meter cannot be created")
+		}
+	}
+
+	return nil, nil
+}
+
+// Transaction makes payment of X units from A to B
+func (t *ReportChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	if function != "report" {
+		return nil, errors.New("Unimplemented '" + function + "' invoked")
+	}
+
+	var name string    // Entities
+	var val int // Asset holdings
+	var err error
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+
+	name = args[0]
+	val, _ = strconv.Atoi(string(args[1]))
+
+	err = stub.PutState(name, []byte(strconv.Itoa(val)))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+
+// Query callback representing the query of a chaincode
+func (t *ReportChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	if function != "query" {
+		return nil, errors.New("Invalid query function name. Expecting \"query\"")
+	}
+	var name string // Entities
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the Meter to query")
+	}
+
+	name = args[0]
+
+	// Get the state from the ledger
+	value, err := stub.GetState(name)
+	if err != nil {
+		jsonResp := "{\"Error\":\"Failed to get state for " + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	if value == nil {
+		jsonResp := "{\"Error\":\"Nil amount for Meter" + name + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	jsonResp := "{\"Name\":\"" + name + "\",\"Amount\":\"" + string(value) + "\"}"
+	fmt.Printf("Query Response:%s\n", jsonResp)
+	return value, nil
+}
+
+func main() {
+	err := shim.Start(new(ReportChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
